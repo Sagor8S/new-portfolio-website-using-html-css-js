d6e1e362737c47a2f32e8361ff759960fb172528
Sagor8S <sagorhowlader.edu@gmail.com>
2026-03-01 07:51:24 +0600

    added an js code start here

--- js/script.js
+++ js/script.js
@@ -8,10 +8,21 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.screenY;
+        let offset = sec.offsetTop - 100;
+        let header = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top <offset + height) {
+            // active navbar Links
+        }
+    });
 
     // sticky header
+    let header = document.querySelector('header');
 
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
